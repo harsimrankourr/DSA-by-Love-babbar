f6ecbf95be4aaabf311c54a5ec781d1a67958d90
harsimrankourr <harsimrankour48@gmail.com>
2026-05-05 21:50:09 +0530

    For loop

---
+++ Intro to C++ Programming/6.Bitwise_operators.cpp
@@ -0,0 +1,58 @@
+
+//Bitwise operator
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   int a = 4;
+   int b = 6;
+
+
+   cout << " a&b " << (a&b) << endl;
+   cout << " a|b " << (a|b) << endl;
+   cout << " ~a " << (~a) << endl;
+   cout << " a^b " << (a^b) << endl;
+}
+
+//==========================================
+
+//Left / Right shift operator 
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   int a = 4;
+   int b = 6;
+
+
+   cout << (17>>1) <<endl;
+   cout << (17>>2) <<endl;
+   cout << (19<<1) <<endl;
+   cout << (21<<2) <<endl;
+}
+
+//===========================================
+
+//Increment and Decrement (imp)
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   int i = 7;
+   
+
+
+   cout << (++i) <<endl;
+   cout << (i++) <<endl;
+   cout << (i--) <<endl;
+   cout << (--i) <<endl;
+}
---
+++ Intro to C++ Programming/7.For_loop.cpp
@@ -0,0 +1,162 @@
+
+//With initialization, condition and updation- 
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   int n;
+   cout << "enter the value of n "<< endl;
+   cin >> n;
+
+
+    cout << "printing count from 1 to n" << endl;
+
+
+    int i = 1;
+for (int i = 1; i<=n; i++){
+        cout << i << endl;
+	i++;
+    }
+}
+
+//====================================================
+
+//Without initialisation, condition and updation-
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   int n;
+   cout << "enter the value of n "<< endl;
+   cin >> n;
+
+
+    cout << "printing count from 1 to n" << endl;
+
+
+    int i = 1;
+    for (; ;){
+        if (i<=n){
+            cout << i << endl;
+        }
+        else {
+            break;
+        }
+        i++;
+    }
+}
+
+//====================================================
+
+//Sum of n numbers
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   int n;
+
+
+   cout << "enter the value of n "<< endl;
+   cin >> n;
+
+
+    int sum = 0;
+   
+    for (int i = 1; i <= n; i++ ){
+        sum += i;
+    }
+    cout << sum << endl;
+}
+
+//=====================================================
+
+//Fibonacci series
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   int n = 10;
+   int a = 0;
+   int b = 1;
+   cout << a <<" "<< b << "";
+
+
+   for (int i = 1; i<=n; i++){
+    int nextNumber = a+b;
+    cout << nextNumber << " ";
+
+
+    a = b;
+    b = nextNumber;
+   }
+   
+}
+
+//=====================================================
+
+//Given number is a prime or not 
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   int n;
+   cout << "enter the value of n " << endl;
+   cin >> n;
+
+
+   bool isPrime = 1;
+   
+   for (int i = 2; i<n; i++){
+    if(n%i == 0){
+        cout << "not a prime number" << endl;
+        isPrime = 0;
+        break;
+    }
+   
+   }
+   if (isPrime == 0){
+    cout <<  "not a prime number" << endl;
+   }
+   else  {
+    cout << "is a prime number "<< endl;
+   }    
+}
+
+//===============================================
+
+//Use of continue in code 
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+
+    for (int i=0; i<5; i++){
+
+
+        cout << "hi" << endl;
+        cout << "hey" << endl;
+
+
+    continue;
+    cout << "reply please" << endl;
+    }
+}
+
