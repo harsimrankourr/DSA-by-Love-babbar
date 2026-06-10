9333dc511b4d1bbdfa594840dce7be7b673451c9
harsimrankourr <harsimrankour48@gmail.com>
2026-06-10 12:20:59 +0530

    Recursion

--- Intro to C++ Programming/22.Recursion.cpp
+++ Intro to C++ Programming/22.Recursion.cpp
@@ -1,43 +1,130 @@
-//FACTORIAL OF A NUMBER
+// FACTORIAL OF A NUMBER
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+int factorial(int n)
+{
 
-int factorial (int n){
+    // base case
+    if (n == 0)
+        return 1;
 
-
-    //base case
-    if( n == 0)
-    return 1;
-
-
-    int smallerProblem = factorial (n-1);
-    int biggerProblen = n*smallerProblem;
-
+    int smallerProblem = factorial(n - 1);
+    int biggerProblem = n * smallerProblem;
 
     return biggerProblem;
-
-
 }
 
-
-int main(){
-
+int main()
+{
 
     int n;
     cin >> n;
 
-
-    int ans = factorial (n);
-
+    int ans = factorial(n);
 
     cout << ans << endl;
-
 
     return 0;
 }
 
-
 //===============================================================
 
+// Power of 2
+
+#include <iostream>
+using namespace std;
+
+int power(int n)
+{
+
+    // base case
+    if (n == 0)
+        return 1;
+
+    // recursive relation
+    int smallerProblem = power(n - 1);
+    int biggerProblem = 2 * smallerProblem;
+
+    return biggerProblem;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int ans = power(n);
+
+    cout << ans << endl;
+
+    return 0;
+}
+
+//===========================================================
+
+// Tall recursion(reverse counting)
+
+#include <iostream>
+using namespace std;
+
+void print(int n)
+{
+    // base case
+    if (n == 0)
+    {
+        return;
+    }
+
+    cout << n << endl;
+
+    // recursive relation
+    print(n - 1);
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    cout << endl;
+    print(n);
+
+    return 0;
+}
+
+//==================================================================
+
+// Head recursion
+
+#include <iostream>
+using namespace std;
+
+void print(int n)
+{
+    // base case
+    if (n == 0)
+    {
+        return;
+    }
+
+    // recursive relation
+    print(n - 1);
+
+    cout << n << endl;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    cout << endl;
+    print(n);
+
+    return 0;
+}
