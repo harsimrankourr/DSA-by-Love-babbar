d7def498b0a9480edcc177ebf96e59fe4d2136f5
harsimrankourr <harsimrankour48@gmail.com>
2026-06-13 20:55:03 +0530

    Recursion with strings

--- Intro to C++ Programming/25.Recursion_with_string.cpp
+++ Intro to C++ Programming/25.Recursion_with_string.cpp
@@ -77,8 +77,8 @@ int main()
 
 //========================================================================
 
-//power of a number 
-//optimised way
+// power of a number
+// optimised way
 
 #include <iostream>
 using namespace std;
@@ -121,3 +121,44 @@ int main()
 
     return 0;
 }
+
+//======================================================================
+
+// Bubble sort
+
+#include <iostream>
+using namespace std;
+
+void sortArray(int *arr, int n)
+{
+
+    // base case
+    if (n == 0 || n == 1)
+    {
+        return;
+    }
+    // 1st case solved - largest number last te pohnch jauga
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+    sortArray(arr, n - 1);
+}
+int main()
+{
+
+    int arr[5] = {2, 5, 1, 6, 9};
+
+    sortArray(arr, 5);
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+//====================================================================
