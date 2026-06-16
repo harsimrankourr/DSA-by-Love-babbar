275c0c8c171344726a633c7bf80771e820664d52
harsimrankourr <harsimrankour48@gmail.com>
2026-06-17 01:47:21 +0530

    Inheritance Ambiguity

--- Intro to C++ Programming/31.4_pillars_of_OOP's/2.Inheritance.cpp
+++ Intro to C++ Programming/31.4_pillars_of_OOP's/2.Inheritance.cpp
@@ -267,3 +267,43 @@ int main()
 
 //==============================================================================
 
+// Inheritance Ambiguity
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+
+public:
+    void func()
+    {
+        cout << "I am A" << endl;
+    }
+};
+
+class B
+{
+
+public:
+    void func()
+    {
+        cout << "I am B" << endl;
+    }
+};
+
+class C : public A, public B
+{
+};
+
+int main()
+{
+
+    C obj;
+    // obj.func();
+
+    obj.A::func();
+    obj.B::func();
+
+    return 0;
+}
