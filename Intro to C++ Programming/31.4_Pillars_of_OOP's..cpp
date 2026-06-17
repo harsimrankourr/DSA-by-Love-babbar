b27482df4e1a384810806792291b4b6a1768025a
harsimrankourr <harsimrankour48@gmail.com>
2026-06-17 23:14:19 +0530

    function and operator overloading of compile time polymorphism

--- Intro to C++ Programming/31.4_Pillars_of_OOP's..cpp
+++ Intro to C++ Programming/31.4_Pillars_of_OOP's..cpp
@@ -314,6 +314,7 @@ int main()
 
 // Inheritance Ambiguity
 
+/*
 #include <iostream>
 using namespace std;
 
@@ -352,13 +353,15 @@ int main()
 
     return 0;
 }
+
+*/
 //===================================================================================
 
 /*
-    *Polymorphism - poly means many and morphs means forms 
-        so polymorphism means existing in multiple forms 
+    *Polymorphism - poly means many and morphs means forms
+        so polymorphism means existing in multiple forms
 
-        there are two types of polymorphism 
+        there are two types of polymorphism
             1. compile time polymorphism - compile time te pta lg jaye k kerhi cgeez kis kis form ch exist kr rhi hai
                     it is of two types
                     a. function overloading
@@ -368,3 +371,102 @@ int main()
 
 
 */
+
+// Function oveerloading
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+
+public:
+    void satHello()
+    {
+        cout << "Hello harsimran" << endl;
+    }
+
+    int sayHello(char name)
+    {
+        cout << "Hello harsimran" << name << endl;
+        return 1;
+    }
+
+    void sayHello(string name)
+    {
+        cout << "Hello" << name << endl;
+    }
+};
+
+int main()
+{
+
+    A obj;
+
+    obj.sayHello("harcharan");
+
+    return 0;
+}
+
+//===============================================================================
+
+// Operator overloading
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+
+public:
+    void satHello()
+    {
+        cout << "Hello harsimran" << endl;
+    }
+
+    int sayHello(char name)
+    {
+        cout << "Hello harsimran" << name << endl;
+        return 1;
+    }
+
+    void sayHello(string name)
+    {
+        cout << "Hello" << name << endl;
+    }
+};
+
+class B
+{
+public:
+    int a;
+    int b;
+
+public:
+    int add()
+    {
+        return a + b;
+    }
+
+    void operator+(B &obj)
+    {
+        int value1 = this->a;
+        int value2 = obj.a;
+        cout << "output" << value2 - value1 << endl;
+    }
+};
+
+int main()
+{
+
+    B obj1, obj2;
+    obj1.a = 4;
+    obj2.a = 7;
+
+    obj1 + obj2;
+
+    return 0;
+}
+
+
+//==============================================================================
