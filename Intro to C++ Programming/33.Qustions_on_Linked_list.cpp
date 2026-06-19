b6052201e935cf8a535a46bb644d60fa4a5dd630
harsimrankourr <harsimrankour48@gmail.com>
2026-06-20 02:47:07 +0530

    Detect a Loop in linked list

--- Intro to C++ Programming/33.Qustions_on_Linked_list.cpp
+++ Intro to C++ Programming/33.Qustions_on_Linked_list.cpp
@@ -334,4 +334,143 @@ int main()
 }
 
 //=========================================================================================================
- 
+
+/*
+// IMPORTANT QUESTIONS
+    - Detect cycle in linkedd list
+    - Remove cycle from linked list
+    - Beginning/ start node of loop in linked list
+*/
+
+//Detect a Loop in linked list
+//============================
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // destructor
+    ~Node()
+    {
+        int value = this->data;
+
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+
+        cout << "Memory is free for node with data " << value << endl;
+    }
+};
+
+void insertNode(Node *&tail, int element, int d)
+{
+    // Empty list
+    if (tail == NULL)
+    {
+        Node *newNode = new Node(d);
+        tail = newNode;
+        newNode->next = newNode;
+    }
+    else
+    {
+        // Non-empty list
+        Node *curr = tail;
+
+        while (curr->data != element)
+        {
+            curr = curr->next;
+        }
+
+        Node *temp = new Node(d);
+        temp->next = curr->next;
+        curr->next = temp;
+    }
+}
+
+void print(Node *tail)
+{
+    if (tail == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    Node *temp = tail;
+
+    do
+    {
+        cout << tail->data << " ";
+        tail = tail->next;
+    } while (tail != temp);
+
+    cout << endl;
+}
+
+bool detectLoop(Node *head)
+{
+    if (head == NULL)
+        return false;
+
+    map<Node *, bool> visited;
+
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        if (visited[temp] == true)
+        {
+            cout << "Loop detected at node: " << temp->data << endl;
+            return true;
+        }
+
+        visited[temp] = true;
+        temp = temp->next;
+    }
+
+    return false;
+}
+
+int main()
+{
+    Node *tail = NULL;
+
+    insertNode(tail, 5, 3);
+    insertNode(tail, 3, 5);
+    insertNode(tail, 5, 7);
+    insertNode(tail, 5, 6);
+    insertNode(tail, 3, 4);
+
+    cout << "Circular Linked List: ";
+    print(tail);
+
+    // Head of circular linked list
+    Node *head = tail->next;
+
+    if (detectLoop(head))
+    {
+        cout << "Cycle is present" << endl;
+    }
+    else
+    {
+        cout << "No cycle present" << endl;
+    }
+
+    return 0;
+}
+
+//=========================================================================================================
