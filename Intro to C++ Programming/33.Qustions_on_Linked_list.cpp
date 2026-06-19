10a5e6f1eb9b770e40fb396da5f93cbafe37863d
harsimrankourr <harsimrankour48@gmail.com>
2026-06-20 01:11:57 +0530

    Linked list reverse in K groups

--- Intro to C++ Programming/33.Qustions_on_Linked_list.cpp
+++ Intro to C++ Programming/33.Qustions_on_Linked_list.cpp
@@ -103,4 +103,42 @@ return temp;
 
 //there is an optimised soln is lse
 
+//================================================================================
+
+//Linked list reverse in K groups 
+//=================================
+
+Node* kReverse (Node* head, int k){
+
+//base case
+if(head == NULL){
+    return NULL;
+}
+
+//step 1 - reverse 1st k nodes
+Node* next = NULL;
+Node* curr = head;
+Node* prev = NULL;
+int coint = 0;
+
+while (curr != NULL && count < k){
+    next = curr -> next;
+    curr -> next = prev;
+    prev = curr;
+    curr = next;
+    count++ ;
+}
+
+//step2 - Baki recursion naal ho jana 
+
+if (next != NULL){
+    head -> next = kReverse(next, k);
+}
+
+//step3 - return head of the reversed linked list
+
+return prev;
+}
+
+
 */
