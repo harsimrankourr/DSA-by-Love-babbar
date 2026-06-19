dcfb7f4e252d8910e2a89ae82f0e26b0a19d7e8a
harsimrankourr <harsimrankour48@gmail.com>
2026-06-19 22:15:11 +0530

    Insertion in circular linked list

--- Intro to C++ Programming/32.Linked_list.cpp
+++ Intro to C++ Programming/32.Linked_list.cpp
@@ -332,6 +332,7 @@ int main()
 
 // Insertion and deletion in Doubly linked list
 
+/*
 #include <iostream>
 using namespace std;
 
@@ -554,4 +555,103 @@ int main()
     return 0;
 }
 
+*/
 //=========================================================================================
+
+// Circular linked list
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // destructor
+    ~Node()
+    {
+        int value = this->data;
+        // memory free
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+        cout << "memory is free for node withd data " << value << endl;
+    }
+};
+
+void insertNode(Node *&tail, int element, int d)
+{
+
+    // assuming that the element is present in the list
+
+    // empty list
+    if (tail == NULL)
+    {
+        Node *newNode = new Node(d);
+        tail = newNode;
+        newNode->next = newNode;
+    }
+    else
+    {
+        // non empty list
+        // assuming that the element  is present in the list
+
+        Node *curr = tail;
+
+        while (curr->data != element)
+        {
+            curr = curr->next;
+        }
+
+        // element found -> curr is representing element wala node
+        Node *temp = new Node(d);
+        temp->next = curr->next;
+        curr->next = temp;
+    }
+}
+
+void print(Node *tail)
+{
+
+    Node *temp = tail;
+
+    do
+    {
+        cout << tail->data << " ";
+        tail = tail->next;
+    } while (tail != temp);
+}
+
+int main()
+{
+
+    Node *tail = NULL;
+
+    insertNode(tail, 5, 3);
+    print(tail);
+
+    insertNode(tail, 3, 5);
+    print(tail);
+
+    insertNode(tail, 5, 7);
+    print(tail);
+
+    insertNode(tail, 5, 6);
+    print(tail);
+
+    insertNode(tail, 3, 4);
+    print(tail);
+
+    return 0;
+}
