00b42422c097a8d80daccb3f3372cd81e5689a42
harsimrankourr <harsimrankour48@gmail.com>
2026-06-20 00:00:20 +0530

    middle of a linked list

--- Intro to C++ Programming/33.Qustions_on_Linked_list.cpp
+++ Intro to C++ Programming/33.Qustions_on_Linked_list.cpp
@@ -1,6 +1,7 @@
 /*
 
 # Reverse the linked list
+//========================
 
 Logic
 
@@ -64,6 +65,40 @@ head -> next -> next = head;
 head -> next = NULL;
 
 return chotaHead;
+}
+//===============================================================================
+
+//Middle of linked list
+//=====================
+
+int getLength(Node* head){
+
+int len = 0;
+while(head != NULL){
+len++;
+head = head -> next;
+}
+return len;
+}
+
+Node*findMiddle(Node* head){
+
+int len = getLength(head);
+//for position
+int ans = (len/2) +1;
+
+//for value of node
+int ans = (len/2);
+
+Node* temp = head;
+
+int cnt = 0;
+while(cnt < ans){
+temp = temp -> next;
+cnt++;
+}
+
+return temp;
 }
 
 */
