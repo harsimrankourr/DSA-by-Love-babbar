f75d7515d41e7392d5d985a9bbef1260573f57d1
harsimrankourr <harsimrankour48@gmail.com>
2026-06-19 23:30:39 +0530

    recursive logic of reverse a linked list

--- Intro to C++ Programming/33.Qustions_on_Linked_list.cpp
+++ Intro to C++ Programming/33.Qustions_on_Linked_list.cpp
@@ -22,5 +22,29 @@ if(head == NULL || head -> next == NULL){
 return head;
 }
 
+//===========================================================================
+
+//Recursive solution
+
+void reverse(Node* &head, Node* curr, Node* prev){
+
+//base case
+if(curr == NULL){
+head = prev;
+return;
+}
+
+Node* forward = curr -> next;
+reverse(head, forward, curr);
+curr -> next = prev
+}
+
+Npde* reverseLinkedList (Node* head){
+
+Node* curr = head;
+Node* prev = NULL;
+reverse(head, curr, prev);
+return head;
+}
 
 */
