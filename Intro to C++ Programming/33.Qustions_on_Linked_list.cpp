fce4b01661aed85bce90a34d34416e877400a0dc
harsimrankourr <harsimrankour48@gmail.com>
2026-06-20 23:26:21 +0530

    Get starting Node of the loop

--- Intro to C++ Programming/33.Qustions_on_Linked_list.cpp
+++ Intro to C++ Programming/33.Qustions_on_Linked_list.cpp
@@ -342,7 +342,7 @@ int main()
     - Beginning/ start node of loop in linked list
 */
 
-//Detect a Loop in linked list
+// Detect a Loop in linked list
 //============================
 
 #include <iostream>
@@ -474,3 +474,377 @@ int main()
 }
 
 //=========================================================================================================
+
+// FLOYD'S CYCLE DETECTION ALGORITHM
+
+//when slow is in the starting of loop and fast is already in the loop, so if after every one iteration of the slow and fast distance between them decrease it means loop/cycle is present in the loop
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // destructor
+    ~Node()
+    {
+        int value = this->data;
+
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+
+        cout << "Memory is free for node with data " << value << endl;
+    }
+};
+
+void insertNode(Node *&tail, int element, int d)
+{
+    // Empty list
+    if (tail == NULL)
+    {
+        Node *newNode = new Node(d);
+        tail = newNode;
+        newNode->next = newNode;
+    }
+    else
+    {
+        // Non-empty list
+        Node *curr = tail;
+
+        while (curr->data != element)
+        {
+            curr = curr->next;
+        }
+
+        Node *temp = new Node(d);
+        temp->next = curr->next;
+        curr->next = temp;
+    }
+}
+
+void print(Node *tail)
+{
+    if (tail == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    Node *temp = tail;
+
+    do
+    {
+        cout << tail->data << " ";
+        tail = tail->next;
+    } while (tail != temp);
+
+    cout << endl;
+}
+
+bool detectLoop(Node *head)
+{
+    if (head == NULL)
+        return false;
+
+    map<Node *, bool> visited;
+
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        if (visited[temp] == true)
+        {
+            cout << "Loop detected at node: " << temp->data << endl;
+            return true;
+        }
+
+        visited[temp] = true;
+        temp = temp->next;
+    }
+
+    return false;
+}
+
+Node *floydDetectLoop(Node *head)
+{
+
+    if (head == NULL)
+        return NULL;
+
+    Node *slow = head;
+    Node *fast = head;
+
+    while (slow != NULL && fast != NULL)
+    {
+
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+        }
+
+        slow = slow->next;
+
+        if (slow == fast)
+        {
+            cout << "Present at " << slow -> data << endl;
+            return slow;
+        }
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    Node *tail = NULL;
+
+    insertNode(tail, 5, 3);
+    insertNode(tail, 3, 5);
+    insertNode(tail, 5, 7);
+    insertNode(tail, 5, 6);
+    insertNode(tail, 3, 4);
+
+    cout << "Circular Linked List: ";
+    print(tail);
+
+    // Head of circular linked list
+    Node *head = tail->next;
+
+    if (detectLoop(head))
+    {
+        cout << "Cycle is present" << endl;
+    }
+    else
+    {
+        cout << "No cycle present" << endl;
+    }
+
+    if (floydDetectLoop(head) != NULL)
+    {
+        cout << "Cycle is pressent " << endl;
+    }
+    else
+    {
+        cout << "No cycle" << endl;
+    }
+
+    return 0;
+}
+
+//=====================================================================================
+
+//Get starting Node of the loop
+
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // destructor
+    ~Node()
+    {
+        int value = this->data;
+
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+
+        cout << "Memory is free for node with data " << value << endl;
+    }
+};
+
+void insertNode(Node *&tail, int element, int d)
+{
+    // Empty list
+    if (tail == NULL)
+    {
+        Node *newNode = new Node(d);
+        tail = newNode;
+        newNode->next = newNode;
+    }
+    else
+    {
+        // Non-empty list
+        Node *curr = tail;
+
+        while (curr->data != element)
+        {
+            curr = curr->next;
+        }
+
+        Node *temp = new Node(d);
+        temp->next = curr->next;
+        curr->next = temp;
+    }
+}
+
+void print(Node *tail)
+{
+    if (tail == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    Node *temp = tail;
+
+    do
+    {
+        cout << tail->data << " ";
+        tail = tail->next;
+    } while (tail != temp);
+
+    cout << endl;
+}
+
+bool detectLoop(Node *head)
+{
+    if (head == NULL)
+        return false;
+
+    map<Node *, bool> visited;
+
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        if (visited[temp] == true)
+        {
+            cout << "Loop detected at node: " << temp->data << endl;
+            return true;
+        }
+
+        visited[temp] = true;
+        temp = temp->next;
+    }
+
+    return false;
+}
+
+Node *floydDetectLoop(Node *head)
+{
+
+    if (head == NULL)
+        return NULL;
+
+    Node *slow = head;
+    Node *fast = head;
+
+    while (slow != NULL && fast != NULL)
+    {
+
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+        }
+
+        slow = slow->next;
+
+        if (slow == fast)
+        {
+            cout << "Present at " << slow -> data << endl;
+            return slow;
+        }
+    }
+
+    return NULL;
+}
+
+Node* getStartingNode(Node* head) {
+
+    if(head == NULL) 
+        return NULL;
+
+    Node* intersection = floydDetectLoop(head);
+    
+    if(intersection == NULL)
+        return NULL;
+    
+    Node* slow = head;
+
+    while(slow != intersection) {
+        slow = slow -> next;
+        intersection = intersection -> next;
+    }  
+
+    return slow;
+
+}
+
+int main()
+{
+    Node *tail = NULL;
+
+    insertNode(tail, 5, 3);
+    insertNode(tail, 3, 5);
+    insertNode(tail, 5, 7);
+    insertNode(tail, 5, 6);
+    insertNode(tail, 3, 4);
+
+    cout << "Circular Linked List: ";
+    print(tail);
+
+    // Head of circular linked list
+    Node *head = tail->next;
+
+
+    //Detection of loop in linked list
+    if (detectLoop(head))
+    {
+        cout << "Cycle is present" << endl;
+    }
+    else
+    {
+        cout << "No cycle present" << endl;
+    }
+
+    if (floydDetectLoop(head) != NULL)
+    {
+        cout << "Cycle is pressent " << endl;
+    }
+    else
+    {
+        cout << "No cycle" << endl;
+    }
+
+    //get starting of loop in the linked list
+
+    Node* loop =  getStartingNode(head);
+    cout << "Loop starts at " << loop -> data << endl;
+
+
+    return 0;
+}
+
+//=====================================================================================================
