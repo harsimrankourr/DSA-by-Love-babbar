cd17979e48b747adde8cb5d0db932d1e98990f88
harsimrankourr <harsimrankour48@gmail.com>
2026-06-23 02:35:22 +0530

    insert an element at the bottom of the stack

--- Intro to C++ Programming/34.Stacks.cpp
+++ Intro to C++ Programming/34.Stacks.cpp
@@ -303,3 +303,31 @@ bool isValidParenthesis(string expression)
         return false;
 }
 //======================================================================
+
+//insert an element at the bottom of the stack
+//============================================
+
+void solve(stack<int>& s, int x) {
+    //base case
+    if(s.empty()) {
+        s.push(x);
+        return ;
+    }
+    
+    int num = s.top();
+    s.pop();
+    
+    //recursive call
+    solve(s, x);
+    
+    s.push(num);
+}
+
+
+stack<int> pushAtBottom(stack<int>& myStack, int x) 
+{
+    solve(myStack, x);
+    return myStack;
+}
+
+//======================================================================
