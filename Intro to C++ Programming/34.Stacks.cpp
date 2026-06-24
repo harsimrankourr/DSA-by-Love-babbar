960632903e32d6b062aa89697cf17bb5b8e8d4ef
harsimrankourr <harsimrankour48@gmail.com>
2026-06-25 02:07:59 +0530

    N stack in an array

--- Intro to C++ Programming/34.Stacks.cpp
+++ Intro to C++ Programming/34.Stacks.cpp
@@ -596,7 +596,7 @@ public:
 
 //The celebrity problem
 //=====================
-
+/*
 //Initial template for C++
 
 #include<bits/stdc++.h>
@@ -697,7 +697,7 @@ int main()
     }
 }
   // } Driver Code Ends
-
+*/
   //====================================================================
 
   //max rectangle
@@ -820,3 +820,94 @@ int main() {
     }
 }
   // } Driver Code Ends
+
+//========================================================================
+
+//N stacks in an array
+//====================
+
+class NStack
+{
+    int *arr;
+    int *top;
+    int *next;
+    
+    int n, s;
+     
+    int freespot;
+    
+public:
+    // Initialize your data structure.
+    NStack(int N, int S)
+    {
+        n = N;
+        s = S;
+        arr = new int[s];
+        top = new int[n];
+        next = new int[s];
+        
+        //top initialise
+        for(int i=0; i<n; i++) {
+            top[i] = -1;
+        }
+        
+        //next initialise
+        for(int i=0; i<s; i++) {
+            next[i] = i+1;
+        }
+        //update last index value to -1
+        next[s-1] = -1;
+        
+        //initialise freespot
+        freespot = 0;
+        
+    }
+
+    // Pushes 'X' into the Mth stack. Returns true if it gets pushed into the stack, and false otherwise.
+    bool push(int x, int m)
+    {
+        //check for overflow
+        if(freespot == -1) {
+            return false;
+        }
+        
+        //find index
+        int index = freespot;
+        
+        //insert element into array
+        arr[index] = x;
+        
+        //update freespot
+        freespot = next[index];
+       
+        //update next;
+        next[index] = top[m-1];
+        
+        //update top
+        top[m-1] = index;
+        
+        return true;
+    }
+
+    // Pops top element from Mth Stack. Returns -1 if the stack is empty, otherwise returns the popped element.
+    int pop(int m)
+    {
+        //check underflow condition
+        if(top[m-1] == -1) {
+            return -1;
+        }
+        
+        int index= top[m-1];
+        
+        top[m-1] = next[index];
+        
+        next[index] = freespot;
+        
+        freespot = index;
+        
+        return arr[index];
+    }
+};
+
+//===================================================================================
+
