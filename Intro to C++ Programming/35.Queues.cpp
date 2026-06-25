88e527032f28ca4d976fe6fe0301e34e1203acfc
harsimrankourr <harsimrankour48@gmail.com>
2026-06-26 01:31:46 +0530

    Queue intro

--- Intro to C++ Programming/35.Queues.cpp
+++ Intro to C++ Programming/35.Queues.cpp
@@ -1,60 +1,58 @@
-class CircularQueue{
-    int *arr;
-    int front;
-    int rear;
-    int size;
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+    cout << (-1)%1<< endl;
+    deque<int> d;
+
+    d.push_front(12);
+    d.push_back(14);
+
+    cout << d.front() << endl;
+    cout << d.back() << endl;
+
+    d.pop_front();
     
-    public:
-    // Initialize your data structure.
-    CircularQueue(int n){
-        size = n;
-        arr = new int[size];
-        front = rear = -1;
+    cout << d.front() << endl;
+    cout << d.back() << endl;
+    d.pop_back();
+
+    if(d.empty()) {
+        cout << "queue is empty" << endl;
+    }
+    else{
+        cout << "queue is not empty " << endl;
     }
 
-    // Enqueues 'X' into the queue. Returns true if it gets pushed into the stack, and false otherwise.
-    bool enqueue(int value){
-        //to check whther queue is full
-        if( (front == 0 && rear == size-1) || (rear == (front-1)%(size-1) ) ) {
-            //cout << "Queue is Full";
-            return false;
-        }
-        else if(front == -1) //first element to push
-        {
-			front = rear = 0;
-            
-        }
-        else if(rear == size-1 && front != 0) {
-            rear = 0; //to maintain cyclic nature
-        }
-        else
-        {//normal flow
-            rear++;
-        }
-        //push inside the queue
-        arr[rear] = value;
-        
-        return true;
-    }
+    /*
+    //create a queue
+    queue<int> q;
 
-    // Dequeues top element from queue. Returns -1 if the stack is empty, otherwise returns the popped element.
-    int dequeue(){
-        if(front == -1){//to check queue is empty
-            //cout << "Queue is Empty " << endl;
-            return -1;
-        }
-        int ans = arr[front];
-        arr[front] = -1;
-        if(front == rear) { //single element is present
-            front = rear = -1;
-        }
-        else if(front == size - 1) {
-            front = 0; //to maintain cyclic nature
-        }
-        else
-        {//normal flow
-            front++;
-        }
-        return ans;
+    q.push(11);
+    cout << "front of q is: " << q.front() << endl;
+    q.push(15);
+    cout << "front of q is: " << q.front() << endl;
+
+    q.push(13);
+    cout << "front of q is: " << q.front() << endl;
+
+    cout << "size of queue is : " << q.size() << endl;
+
+    q.pop();
+    q.pop();
+    q.pop();
+
+    cout << "size of queue is : " << q.size() << endl;
+
+    if(q.empty()) {
+        cout << "Queue is empty " << endl;
     }
-};
+    else{
+        cout << "queue is not empty " << endl;
+    }
+    */
+
+
+    return 0;
+}
