1fa6359f0e62ddda49ca73c56a4dca780e282e7f
harsimrankourr <harsimrankour48@gmail.com>
2026-07-05 02:43:30 +0530

    Two sum in a BST

---
+++ Intro to C++ Programming/41.BST_questions/7.TwoSumInaBST.cpp
@@ -0,0 +1,41 @@
+/*
+
+void inorder(BinaryTreeNode<int> *root, vector<int> &in)
+{
+
+    if (root == NULL)
+        return;
+
+    inorder(root->left, in);
+    in.push_back(root->data);
+    inorder(root->right, in);
+}
+
+bool twoSumInBST(BinaryTreeNode<int> *root, int target)
+{
+    vector<int> inorderVal;
+
+    //Store inorder -> sorted values
+    inorder(root, inorderrVal);
+
+    //use 2 Pointer approach to check if pair exists
+    int i = 0, j = inorderVal.size() - 1;
+
+    while (i < j)
+    {
+        int sum = inorderVal[i] + inorderVal[j];
+
+        if (sum == target)
+            return true;
+
+        else if (sum > target)
+            j--;
+        else
+        {
+            i++;
+        }
+    }
+    return false;
+}
+
+*/
