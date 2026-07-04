fb03d9b5c0ebb1e8fb2977847551632d4f507b90
harsimrankourr <harsimrankour48@gmail.com>
2026-07-04 20:25:25 +0530

    iterative way of Search in BST

--- Intro to C++ Programming/41.BST_questions/1.SearchInBST.cpp
+++ Intro to C++ Programming/41.BST_questions/1.SearchInBST.cpp
@@ -27,3 +27,33 @@ bool searchInBST(BinaryTreeNode<int> *root, int x)
 };
 
 */
+
+// T.c here is O(H) or O(n) in worst case
+// S.C here is O(H)
+
+// if we done this problem in iterative way than recursive way then space complexity will become constant
+
+bool searchInBST(BinaryTreeNode<int> *root, int x)
+{
+
+    BinaryTreeNode<int> *temp = root;
+
+    while (temp != NULL)
+    {
+        if (temp->data == x)
+        {
+            return true;
+        }
+
+        if (temp->data > x)
+        {
+            temp = temp->left;
+        }
+
+        else
+        {
+            temp = temp->right;
+        }
+    }
+    return false;
+};
