b38bbfbe15e4e431159468798c88cbc404021439
harsimrankourr <harsimrankour48@gmail.com>
2026-07-06 23:14:31 +0530

    Basics of heap

---
+++ Intro to C++ Programming/42.Heaps.cpp
@@ -0,0 +1,20 @@
+/*
+
+-> A heap is a specialised, tree - based data  structure that satisfies the heap property 
+    and is structurally a complete binary tree.
+
+-> In other wirds it is a completely binary tree that comes with a heap order property.
+
+-> COMPLETELY BINARY  TREE
+    - every level is completely filled except the last level
+    - nodes always added from the left.
+    - or nodes always lean from left
+
+-> HEAP ORDER PROPERTY
+    - Max heap
+        Child of every node must be smaller than that node.
+
+    - Min heap
+        child of every node must be greater than that node.
+
+*/
