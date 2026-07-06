888c227afed6b2a896c4c6b104d82386cf476437
harsimrankourr <harsimrankour48@gmail.com>
2026-07-07 00:43:08 +0530

    Deletion in heap

--- Intro to C++ Programming/42.Heaps.cpp
+++ Intro to C++ Programming/42.Heaps.cpp
@@ -24,38 +24,88 @@ using namespace std;
 
 class heap
 {
-    public:
+public:
     int arr[100];
     int size = 0;
 
-    void insert(int val){
+    heap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+    void insert(int val)
+    {
 
         size = size + 1;
         int index = size;
         arr[index] = val;
 
-        while(index > 1){
-            int parent = index/2;
+        while (index > 1)
+        {
+            int parent = index / 2;
 
-            if(arr[parent] < arr[index]){
+            if (arr[parent] < arr[index])
+            {
                 swap(arr[parent], arr[index]);
                 index = parent;
             }
-            else{
-                return ;
+            else
+            {
+                return;
             }
         }
-
     }
 
-    void print(){
-        for (int i = 1; i<=size; i++){
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
             cout << arr[i] << " ";
-        }cout << endl;
+        }
+        cout << endl;
+    }
+
+    void deletefromHeap()
+    {
+        if (size == 0)
+        {
+            cout << "nothing to delete " << endl;
+            return;
+        }
+
+        // Step 1. put last element into first index
+        arr[1] = arr[size];
+
+        // Step 2. remove last element
+        size--;
+
+        // Step 3. take root node to its correct position
+
+        int i = 1;
+        while (i < size)
+        {
+            int leftIndex = 2 * i;
+            int rightIndex = 2 * i + 1;
+
+            if (leftIndex < size && arr[i] < arr[leftIndex])
+            {
+                swap(arr[i], arr[leftIndex]);
+            }
+            else if (rightIndex < size && arr[i] < arr[rightIndex])
+            {
+                swap(arr[i], arr[rightIndex]);
+                i = rightIndex;
+            }
+            else
+            {
+                return;
+            }
+        }
     }
 };
 
-int main(){
+int main()
+{
 
     heap h;
     h.insert(50);
@@ -63,7 +113,11 @@ int main(){
     h.insert(53);
     h.insert(52);
     h.insert(54);
+    h.print();  
+
+    h.deletefromHeap();
     h.print();
-    
+    return 0;
+
     return 0;
 }
