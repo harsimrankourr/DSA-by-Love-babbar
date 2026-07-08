57d93d5e47814912daf2f6be694775da8a88641f
harsimrankourr <harsimrankour48@gmail.com>
2026-07-08 23:59:24 +0530

    Merge two binary max heaps

--- Intro to C++ Programming/42.Heaps.cpp
+++ Intro to C++ Programming/42.Heaps.cpp
@@ -114,12 +114,12 @@ void heapify(int arr[], int n, int i)
     int left = 2 * i;
     int right = 2 * i + 1;
 
-    if (left < n && arr[largest] < arr[left])
+    if (left <= n && arr[largest] < arr[left])
     {
         largest = left;
     }
 
-    if (right < n && arr[largest] < arr[right])
+    if (right <= n && arr[largest] < arr[right])
     {
         largest = right;
     }
@@ -149,6 +149,8 @@ int main()
     int arr[6] = {-1, 54, 53, 55, 52, 50};
 
     int n = 5;
+
+    //heap creation
     for (int i = n / 2; i > 0; i++)
     {
         heapify(arr, n, i);
