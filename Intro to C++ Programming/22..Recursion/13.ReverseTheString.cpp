94a748328da4ecb12928503e212e5d388ae1e044
harsimrankourr <harsimrankour48@gmail.com>
2026-07-09 22:49:15 +0530

    Recursion day4

---
+++ Intro to C++ Programming/22..Recursion/13.ReverseTheString.cpp
@@ -0,0 +1,31 @@
+// reverse the string
+
+#include <iostream>
+using namespace std;
+
+void reverse(string &str, int i, int j)
+{
+
+    // base case
+    if (i > j)
+        return;
+
+    swap(str[i], str[j]);
+    i++;
+    j--;
+
+    // recursive call
+    reverse(str, i, j);
+}
+
+int main()
+{
+
+    string name = "harsimran";
+
+    reverse(name, 0, name.length() - 1);
+
+    cout << name << endl;
+
+    return 0;
+}
