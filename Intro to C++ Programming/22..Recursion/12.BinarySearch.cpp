71d0df4a8a44905cf584416ff58fde1b2051d4db
harsimrankourr <harsimrankour48@gmail.com>
2026-07-09 22:45:42 +0530

    Recursion day3

---
+++ Intro to C++ Programming/22..Recursion/12.BinarySearch.cpp
@@ -0,0 +1,46 @@
+
+// binary search with recursion
+
+// first condition of binary search is array should be sorted
+
+#include <iostream>
+using namespace std;
+
+bool binarySearch(int *arr, int s, int e, int k)
+{
+
+    // base case
+    // elemment not found
+    if (s > e)
+    {
+        return false;
+
+        int mid = s + (e - s) / 2;
+
+        // element found
+        if (arr[mid] == k)
+            return true;
+
+        if (arr[mid] < k)
+        {
+            return binarySearch(arr, mid + 1, e, k);
+        }
+        else
+        {
+            return binarySearch(arr, s, mid - 1, k);
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+
+    int arr[6] = {2, 4, 6, 10, 14, 16};
+    int size = 6;
+    int key = 18;
+
+    cout << "present or not " << binarySearch(arr, 0, 5, key) << endl;
+
+    return 0;
+}
