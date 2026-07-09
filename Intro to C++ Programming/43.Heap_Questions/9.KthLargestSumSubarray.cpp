ee0439f36bea88e8afcfddbe17025dc795d36f5d
harsimrankourr <harsimrankour48@gmail.com>
2026-07-09 22:39:14 +0530

    Recursion day1

--- Intro to C++ Programming/43.Heap_Questions/9.KthLargestSumSubarray.cpp
+++ Intro to C++ Programming/43.Heap_Questions/9.KthLargestSumSubarray.cpp
@@ -30,7 +30,8 @@ int getKthLargest(vector<int> &arr, int k)
     int n = arr.size();
     for (int i = 0; i < n; i++)
     {
-        int sum = 0 for (int j = i; j < n; j++)
+        int sum =0;
+        for (int j = i; j < n; j++)
         {
             sum += arr[j];
             if (mini.size() < k)
