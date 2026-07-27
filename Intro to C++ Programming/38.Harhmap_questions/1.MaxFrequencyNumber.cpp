c689d28fc17b15c29e55f74cbcab5490f996179c
harsimrankourr <harsimrankour48@gmail.com>
2026-07-27 16:03:34 +0530

    in general solution of maximum frequent number

---
+++ Intro to C++ Programming/38.Harhmap_questions/1.MaxFrequencyNumber.cpp
@@ -0,0 +1,29 @@
+
+/*
+// ingeneral solution of maximum frequent number
+
+int maximumFrequency(vector<int> &arr, int n)
+{
+
+    unordered_map<int, int> count;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        count[arr[i]++];
+    }
+
+    int maxi = INT_MIN;
+    int ans = -1;
+
+    for (auto i : m)
+    {
+        if (i.second > maxi)
+        {
+            maxi = i.second;
+            ans = i.first;
+        }
+    }
+    return ans;
+}
+    */
+   
