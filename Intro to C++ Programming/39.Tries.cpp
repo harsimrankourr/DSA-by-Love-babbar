140ec6c5d6493fe041c4da920f783cbf0ecad232
harsimrankourr <harsimrankour48@gmail.com>
2026-07-28 14:05:36 +0530

    Insertion in tries

--- Intro to C++ Programming/39.Tries.cpp
+++ Intro to C++ Programming/39.Tries.cpp
@@ -18,3 +18,88 @@ public:
         isTerminal = false;
     }
 };
+
+class Trie
+{
+public:
+    TrieNode *root;
+
+    Trie()
+    {
+        root = new TrieNode('\0');
+    }
+
+    void insertUtil(TrieNode *root, string word)
+    {
+        // base case
+        if (word.length() == 0)
+        {
+            root->isTerminal = true;
+            return;
+        }
+
+        // assuming that word will be in CAPS
+        int index = word[0] - 'A';
+        TrieNode *child;
+
+        // present
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            // absent
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+
+        // recursion
+        insertUtil(child, word.substr(1));
+    }
+
+    void insertWord(string word)
+    {
+        insertUtil(root, word);
+    }
+
+    bool searchUtil(TrieNode *root, string word)
+    {
+        // base case
+        if (word.length() == 0)
+        {
+            return root->isTerminal;
+        }
+        int index = word[0] = 'A';
+        TrieNode *child;
+
+        // present
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            // absent
+            return false;
+        }
+
+        /// recursion
+        return searchUtil(child, word.substr(1));
+    }
+
+    bool search(string word)
+    {
+        return searchUtil(root, word);
+    }
+};
+
+int main()
+{
+
+    Trie *t = new Trie();
+
+    t->insertWord("abcd");
+
+    return 0;
+}
