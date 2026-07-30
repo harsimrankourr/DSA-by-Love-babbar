449a08fdee96194fdefd20248fbb317c69f2d17a
harsimrankourr <harsimrankour48@gmail.com>
2026-07-30 15:17:25 +0530

    Introduction to graphs

---
+++ Intro to C++ Programming/42.IntroToGraphs.cpp
@@ -0,0 +1,37 @@
+/*
+In simple words Graph is a type of data structure which is basically the combination of nodes and edges.
+
+In the other words it is a non linear data structure consisting of a finite set of vertices (also called nodes) and
+a set of edges that connect these nodes to represent relationship.
+
+    Nodes- entities to store data
+    edges - connecting nodes
+    
+Types of graphs:
+    - Directed graphs 
+        - weighted directed graphs
+
+    - Undirected graphs
+        -weighted undirected graphs
+
+If weight is not given, so it is safe  to assume that weight on edge is 1.
+
+Degree - No. of edges passing through that particular node 
+            In case of directed graphs 
+                - Indegree (No. of edges comes toward the node)
+                - Outdegree (Mo. of edges goes from the node).
+
+Path - travel from one node to another.
+
+Cyclic graph - If there will be weight present then
+                - Weighted cyclic directed graph
+                - Weighted cyclic undirected graph
+
+Acyclic graps -If there will be weight present then
+                - Weighted Acyclic directed graph
+                - Weighted Acyclic undirected graph
+
+Graphs
+        - Adjacency matrix 
+        - Adacency list
+*/
