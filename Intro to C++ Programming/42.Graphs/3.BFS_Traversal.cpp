d94ccd6526dd6275a1199f8106cf246cd1cfaf9d
harsimrankourr <harsimrankour48@gmail.com>
2026-07-31 12:44:51 +0530

    BFS traversal using sets

--- Intro to C++ Programming/42.Graphs/3.BFS_Traversal.cpp
+++ Intro to C++ Programming/42.Graphs/3.BFS_Traversal.cpp
@@ -79,3 +79,72 @@ vector<int> BFS(int vertex, vector<pair<int, int>> edges)
     return ans;
 }
     */
+//=================================================================================
+
+//Using sets
+
+/*
+#include <unordered_map>
+#include <queue>
+#include <list>
+#include <set>
+
+void prepareAdjList(unorederd_map<int, set<int>> &adjList, vector<pair<int, int>> &edges)
+{
+    // creating an adjacency list
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = edges[i].first;
+        int v = edges[i].second;
+
+        adjList[u].insert(v);
+        adjList[v].insert(u);
+    }
+}
+
+void bfs(unordered_map<int, set<int>> &adjList, unordered_map<int, bool> &visited, vector<int> &ans, int node)
+{
+    queue<int> q;
+    q.push(node);
+    visited[node] = 1;
+
+    while (!q.empty())
+    {
+        int frontNode = q.front();
+        q.pop();
+
+        // store frontNode into ans
+        ans.push_back(frontNode);
+
+        // traversal all neighbours of front node
+        for (auto i : adjList[frontNode])
+        {
+            if (!visited[i])
+            {
+                q.push(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+vector<int> BFS(int vertex, vector<pair<int, int>> edges)
+{
+    unordered_map<int, set<int>> adjList;
+    vector<int> ans;
+    unordered_map<int, bool> visited;
+
+    prepareAdjList(adjList, edges);
+    // printAdj(adjList);
+
+    // traverse all the cinnecting and disconnecting components of the graph
+    for (int i = 0; i < vertex; i++)
+    {
+        if (!visited[i])
+        {
+            bfs(adjList, visited, ans, i);
+        }
+    }
+    return ans;
+}
+    */
