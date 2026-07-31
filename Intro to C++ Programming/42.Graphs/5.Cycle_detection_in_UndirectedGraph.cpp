d5e57dde66a65e9c03aa6a701e6d2bf7181dc165
harsimrankourr <harsimrankour48@gmail.com>
2026-07-31 16:39:46 +0530

    Cyclic detection in unordered map using DFS

--- Intro to C++ Programming/42.Graphs/5.Cycle_detection_in_UndirectedGraph.cpp
+++ Intro to C++ Programming/42.Graphs/5.Cycle_detection_in_UndirectedGraph.cpp
@@ -53,7 +53,92 @@ string cycleDetection(vector<vector<int>> &edges, int n, int m)
     {
         if (!visited[i])
         {
-            bool ans = (isCyclicBFSi, visited, adj);
+            bool ans = isCyclicBFS (i, visited, adj);
+            is(ans == 1) return "Yes";
+        }
+    }
+    return "No";
+}
+    */
+//================================================================================
+
+// Using DFS
+/*
+#include <unordered_map>
+#include <queue>
+#include <list>
+
+bool isCyclicBFS(int src, unordered_map<int, bool> &visited, unordered_map<int, list<int>> &adj)
+{
+    unordered_map<int, int> parent;
+
+    parent[src] = -1;
+    visited[src] = 1;
+    queue<int> q;
+    q.push(src);
+
+    while (!q.empty())
+    {
+        int front = q.front();
+        q.pop();
+
+        for (auto neighbour : adj[front])
+        {
+            if (visited[neighbour] == true && neighbour != parent[front])
+            {
+                return true;
+            }
+            else if (!visited[neighbour])
+            {
+                q.push(neighbour);
+                visited[neighbour] = 1;
+                parent[neighbour] = front;
+            }
+        }
+    }
+    return false;
+}
+
+bool isCyclicDFS(int node, int parent, unordered_map<int, bool> &visited, unordered_map<int, list<int>> &adj)
+{
+    visited[node] = true;
+
+    for (auto neighbour : adj[node])
+    {
+        if (!visited[neighbour])
+        {
+            bool cycleDetected = isCyclicDFS(neighbour, node, visited, adj);
+            if (cycleDetected)
+                return true;
+        }
+        else if (neighbour != parent)
+        {
+            // cycle present
+            return true;
+        }
+    }
+    return false;
+}
+string cycleDetection(vector<vector<int>> &edges, int n, int m)
+{
+    // create adjacency list
+    unordered_map<int, list<int>> adj;
+    for (int i = 0; i < m; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    // To handle disconnected components
+    unordered_map<int, bool> visited;
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            bool ans = isCyclicDFS(i, -1, visited, adj);
             is(ans == 1) return "Yes";
         }
     }
