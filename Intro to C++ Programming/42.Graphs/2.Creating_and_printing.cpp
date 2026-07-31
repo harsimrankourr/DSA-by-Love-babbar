8906aa5f18c359831ed1e2ce5eee2eed56e0cddf
harsimrankourr <harsimrankour48@gmail.com>
2026-07-31 11:43:53 +0530

    creating and printing of graphs

--- Intro to C++ Programming/42.Graphs/2.Creating_and_printing.cpp
+++ Intro to C++ Programming/42.Graphs/2.Creating_and_printing.cpp
@@ -4,5 +4,25 @@ Given a undirected graph with N nodes and M edges. Have to create a graph and pr
 
 vector<vector<int>> printAdjacency(int n, int m, vector< vector< int>> &edges){
     vector<int> ans[n];
-    for(int i=0; i<m; i++){}
+
+    //ans array will store all adjacent nodes corresponding on indexes.
+    for(int i=0; i<m; i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        ans[u].push_back(v);
+        ans[v].push_back(u);
+    }
+
+    vector<vector<int>> adj;
+    for(int i=0; i<n; i++){
+        //here i storing along with the answer of the i
+        adj[i].push_back(i);
+
+        //entering neighbours
+        for(int j=0; j<ans[i].size(); j++){
+            adj[i].push_back(ans[i][j]);
+        }
+    }
+    return adj;
 }
