2ffc133a09fe948d403ace131adac2b82b01428e
harsimrankourr <harsimrankour48@gmail.com>
2026-07-31 11:47:21 +0530

    creating and printing of adjacency list

--- Intro to C++ Programming/42.Graphs/2.Creating_and_printing.cpp
+++ Intro to C++ Programming/42.Graphs/2.Creating_and_printing.cpp
@@ -2,6 +2,7 @@
 Given a undirected graph with N nodes and M edges. Have to create a graph and print an adjacency list
 */ 
 
+/*
 vector<vector<int>> printAdjacency(int n, int m, vector< vector< int>> &edges){
     vector<int> ans[n];
 
@@ -26,3 +27,4 @@ vector<vector<int>> printAdjacency(int n, int m, vector< vector< int>> &edges){
     }
     return adj;
 }
+    */
