b325daaa77793f96e66a69bfd01299f1a42c4900
harsimrankourr <harsimrankour48@gmail.com>
2026-08-01 14:10:37 +0530

    Topological sort using Kahn's algorithm or BFS

---
+++ Intro to C++ Programming/42.Graphs/8.Topological_sort_using_Kahn's_algorithm.cpp
@@ -0,0 +1,55 @@
+// Topological sort using BFS or Kahn's Algorithm
+/*
+#include <unordered_map>
+#include <list>
+#include <queue>
+
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)
+{
+    // create adj list
+    unordered_map<int, list<int>> adj;
+    for (int i = 0; i < e; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adj[u].push_back(v);
+    }
+
+    // find all the indegrees
+    vector<int> indegree(v);
+    for (auto i : adj)
+    {
+        for (auto j : i.second)
+        {
+            indegree[j]++;
+        }
+    }
+    // insert all the elements having indegree 0 in the queue
+    queue<int> q;
+    for (int i = 0; i < v; i++)
+    {
+        if (indegree[i] == 0)
+            q.push(i;)
+    }
+
+    // do BFS's
+    vector<int> ans;
+    while (!q.empty())
+    {
+        int front = q.front();
+        q.pop();
+
+        // ans store
+        ans.push_back(front);
+
+        // neighbour indegree update
+        for (auto neighbour : adj[front])
+        {
+            indegree[neighbour]--;
+            if (indegree[neighbour] == 0)
+                q.push(neighbour);
+        }
+    }
+    return ans;
+}
+    */
