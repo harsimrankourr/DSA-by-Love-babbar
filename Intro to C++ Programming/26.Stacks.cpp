8dbe9f37649f66cce0033391daeec95f5caae03a
harsimrankourr <harsimrankour48@gmail.com>
2026-08-02 19:06:44 +0530

    Reverse string using stacks

--- Intro to C++ Programming/26.Stacks.cpp
+++ Intro to C++ Programming/26.Stacks.cpp
@@ -134,46 +134,10 @@ int main()
     return 0;
 }
 
-//=====================================================================================
 
-
-///==============================================================================
-
-// Reverse a string using stack
-//============================
-
-#include <iostream>
-#include <stack>
-using namespace std;
-
-int main()
-{
-    string str = "babbar";
-
-    stack<char> s;
-
-    for (int i = 0; i < str.length(); i++)
-    {
-        char ch = str[i];
-        s.push(ch);
-    }
-
-    string ans = "";
-
-    while (!s.empty())
-    {
-        char ch = s.top();
-        ans.push_back(ch);
-
-        s.pop();
-    }
-
-    cout << "answer is: " << ans << endl;
-
-    return 0;
 
     //===========================================================================
-}
+
 // Delete middle element of the stack
 //==================================
 
