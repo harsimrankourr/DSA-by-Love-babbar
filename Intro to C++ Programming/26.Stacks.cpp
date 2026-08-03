610e60936d05edfe169b04d05d5b2e42ba08f6f5
harsimrankourr <harsimrankour48@gmail.com>
2026-08-03 18:59:17 +0530

    Reverse stack using recursion

--- Intro to C++ Programming/26.Stacks.cpp
+++ Intro to C++ Programming/26.Stacks.cpp
@@ -134,58 +134,6 @@ int main()
     return 0;
 }
 
-
-//===========================================================================
-
-
-//======================================================================
-
-// insert an element at the bottom of the stack
-//============================================
-
-
-//======================================================================
-
-// Reverse stack using recursion
-//=============================
-
-void insertAtBottom(stack<int> &s, int element)
-{
-    // basecase
-    if (s.empty())
-    {
-        s.push(element);
-        return;
-    }
-
-    int num = s.top();
-    s.pop();
-
-    // recursive call
-    insertAtBottom(s, element);
-
-    s.push(num);
-}
-
-void reverseStack(stack<int> &stack)
-{
-    // base case
-    if (stack.empty())
-    {
-        return;
-    }
-
-    int num = stack.top();
-    stack.pop();
-
-    // recursive call
-    reverseStack(stack);
-
-    insertAtBottom(stack, num);
-}
-
-//==========================================================================
-
 // sort a stack
 //============
 
