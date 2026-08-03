39037034d38ca8cc26d5bf65a95f3063a7ce6974
harsimrankourr <harsimrankour48@gmail.com>
2026-08-03 16:43:33 +0530

    Kruskal's algorithm

--- Intro to C++ Programming/42.Graphs/14.Kruskal's_Algorithm.cpp
+++ Intro to C++ Programming/42.Graphs/14.Kruskal's_Algorithm.cpp
@@ -58,3 +58,77 @@ int minimumSpannigTree(vector<vector<int>> &edges, int n)
     makeSet(parent, rank, n);
 }
     */
+//====================================================================================
+
+/*
+#include <algorithm>
+
+bool cmp(vector<int> &a, vector<int> &b)
+{
+    // There are wights on edges in the 2nd column
+    return a[2] < b[2];
+}
+
+void makeSet(vector<int> &parent, vector<int> &rank, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        parent[i] = i;
+        rank[i] = 0;
+    }
+}
+
+int findParent(vector<int> &parent, int node)
+{
+    if (parent[node] == node)
+    {
+        return node;
+    }
+
+    return parent[node] = findParent(parent, parent[node]);
+}
+
+void unionSet(int u, int v, vector<int> &parent, vector<int> &rank)
+{
+    u = findParent(parent, u);
+    v = findParent(parent, v);
+
+    if (rank[u] < rank[v])
+    {
+        parent[u] = v;
+    }
+    else if (rank[v] < rank[u])
+    {
+        parent[v] = u;
+    }
+    else
+    {
+        parent[v] = u;
+        rank[u]++;
+    }
+}
+
+int minimumSpannigTree(vector<vector<int>> &edges, int n)
+{
+    sort(edges.beigin(), edges.end(), cmp);
+    vector<int> parent(n);
+    vector<int> rank(n);
+    makeSet(parent, rank, n);
+
+    int minWeight = 0;
+
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = findParent(parent, edges[i][0]);
+        int v = findParent(parent, edges[i][1]);
+        int wt = edges[i][2];
+
+        if (u != v)
+        {
+            minWeight += wt;
+            unionSet(u, v, parent, rank);
+        }
+    }
+    return minWeight;
+}
+    */
