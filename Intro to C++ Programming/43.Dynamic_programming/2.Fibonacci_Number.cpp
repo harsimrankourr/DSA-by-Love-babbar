4e8cf5d55467cbed275a2f04288d9ae7474543c4
harsimrankourr <harsimrankour48@gmail.com>
2026-08-04 18:08:43 +0530

    Fibonacci series Space optimised in DP

--- Intro to C++ Programming/43.Dynamic_programming/2.Fibonacci_Number.cpp
+++ Intro to C++ Programming/43.Dynamic_programming/2.Fibonacci_Number.cpp
@@ -39,6 +39,7 @@ int main()
 // Using bottom up approach - Tabulation
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
@@ -60,3 +61,35 @@ int main()
         cout << dp[n] << endl;
     }
 }
+
+//================================================================
+
+// Space optimization
+
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int prev1 = 1;
+    int prev2 = 0;
+
+    if (n == 0)
+        return prev2;
+
+    for (int i = 2; i <= n; i++)
+    {
+        int cuee = prev1 + prev2;
+        // shift logic
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    cout << prev1 << endl;
+    return 0;
+}
+    */
