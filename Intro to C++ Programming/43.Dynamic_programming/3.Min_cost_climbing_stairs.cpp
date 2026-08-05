d64f977cab6a016bfe75d9a1aa2218c74deb0f08
harsimrankourr <harsimrankour48@gmail.com>
2026-08-05 14:48:29 +0530

    Minimum cost climbing stairs with top down approach and space optimization

--- Intro to C++ Programming/43.Dynamic_programming/3.Min_cost_climbing_stairs.cpp
+++ Intro to C++ Programming/43.Dynamic_programming/3.Min_cost_climbing_stairs.cpp
@@ -1,7 +1,9 @@
 /*
+
 class Solution
 {
 public:
+    // Solve 1 for recursion
     int solve(vector<int> &cost, int n, vector<int> &dp)
     {
         // Base case
@@ -14,6 +16,7 @@ public:
         return ans;
     }
 
+    // Solve 2 for top down approach in Dp
     int solve2(vector<int> &cost, int n, vector<int> &dp)
     {
         // Base case
@@ -31,14 +34,56 @@ public:
         return dp[n];
     }
 
+    // Solve 3 for bottom up approach in dp
+    int solve3(vector<int> &cost, int n)
+    {
+        // Step 1 - creation of a dp array
+        vector<int> dp(n + 1);
+
+        // Step 2 - Analise base case
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        // Step 3 - 0 and 1 is handled by base case, handle the remaining from 2 to n
+        for (int i = 2; i < n; i++)
+        {
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+        }
+        return min(dp[n - 1], dp[n - 2]);
+    }
+
+    // Solve 4 for space optimization in dp
+    int solve4(vector<int> &cost, int n)
+    {
+        int prev2 = cost[0];
+        int prev1 = cost[1];
+
+        for (int i = 2; i < n; i++)
+        {
+            int curr = cost[i] + min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return min(prev1, prev2);
+    }
+
     int minCostClimbingStairs(vector<int> &cost)
     {
-        int n = cost.size();
+        // // For top down approach
+        // int n = cost.size();
 
-        // Step 1 - create a Dp array and  initialise it with -1
-        vector<int> dp(n + 1, -1);
-        int ans = min(solve2(cost, n - 1, dp), solve2(cost, n - 2, dp));
-        return ans;
+        // // Step 1 - create a Dp array and  initialise it with -1
+        // vector<int> dp(n + 1, -1);
+        // int ans = min(solve2(cost, n - 1, dp), solve2(cost, n - 2, dp));
+        // return ans;
+
+        // // For bottom up approach
+        // int n = cost.size();
+        // return solve3(cost, n);
+
+        // For space optimization
+        int n = cost.size();
+        return solve4(cost, n);
     }
 };
 */
