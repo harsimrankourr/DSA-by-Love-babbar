9260192be34c8d46f72c99871364dad44647c30a
harsimrankourr <harsimrankour48@gmail.com>
2026-08-10 18:44:48 +0530

    Knapsack problem space optimal solution

--- Intro to C++ Programming/43.Dynamic_programming/10.Knapsack_problem.cpp
+++ Intro to C++ Programming/43.Dynamic_programming/10.Knapsack_problem.cpp
@@ -14,6 +14,7 @@ Brute force approach comes in the mind
      Similar as subsequences question in the recursion.
 */
 
+/*
 // Brute force
 int solve(vector<int> &weight, vector<int> &value, int index, int w)
 {
@@ -79,7 +80,7 @@ int solveTab(vector<int> &weight, vector<int> &value, int index, int capacity)
             dp[0][w] = 0;
     }
 
-    //Take care of remaining recursive calls 
+    // Take care of remaining recursive calls
     for (int index = 1; index < n; index++)
     {
         for (int w = 0; w <= capacity; w++)
@@ -97,6 +98,41 @@ int solveTab(vector<int> &weight, vector<int> &value, int index, int capacity)
     return dp[n - 1][capacity];
 }
 
+int solveTabSO(vector<int> &weight, vector<int> &value, int index, int capacity)
+{
+    // Step 1 creating 2 vectors
+
+    vector<int> prev(capacity + 1, 0);
+    vector<int> curr(capacity + 1, 0);
+
+    // Step 2 Trying to ananlyse the base case
+    for (int w = weight[0]; w <= capacity; w++)
+    {
+        if (weight[0] <= capacity)
+            prev[w] = value[0];
+        else
+            prev[w] = 0;
+    }
+
+    // Take care of remaining recursive calls
+    for (int index = 1; index < n; index++)
+    {
+        for (int w = 0; w <= capacity; w++)
+        {
+            int include = 0;
+            if (weight[index] <= w)
+                include = value[index] + prev[w - weight[index]];
+
+            int exclude = 0 + prev[w];
+
+            // Storing ans in dp
+            curr[w] = max(exclude, include);
+        }
+        prev = curr;
+    }
+    return prev[capacity];
+}
+
 int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
 {
     // return solve(weight, value, n - 1, maxWeight);
@@ -105,3 +141,4 @@ int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
     vector<vector<int>> dp(n, vector<int>(maxWeight + 1, -1));
     return solveMemo(weight, value, n-- 1, maxWeight, dp);
 }
+    */
