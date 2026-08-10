d93e21e223dcd01a9864582e02f7d45ef10a15c1
harsimrankourr <harsimrankour48@gmail.com>
2026-08-10 18:39:20 +0530

    Knapsack problem using Tabulation

--- Intro to C++ Programming/43.Dynamic_programming/10.Knapsack_problem.cpp
+++ Intro to C++ Programming/43.Dynamic_programming/10.Knapsack_problem.cpp
@@ -64,6 +64,38 @@ int solveMemo(vector<int> &weight, vector<int> &value, int index, int capacity, 
     return dp[index][capacity];
 }
 
+int solveTab(vector<int> &weight, vector<int> &value, int index, int capacity)
+{
+    // Step 1 creating a dp array
+
+    vector<vector<int>> dp(n, vector<int>(maxWeight + 1, 0));
+
+    // Step 2 Trying to ananlyse the base case
+    for (int w = weight[0]; w <= capacity; w++)
+    {
+        if (weight[0] <= capacity)
+            dp[0][w] = value[0];
+        else
+            dp[0][w] = 0;
+    }
+
+    //Take care of remaining recursive calls 
+    for (int index = 1; index < n; index++)
+    {
+        for (int w = 0; w <= capacity; w++)
+        {
+            int include = 0;
+            if (weight[index] <= w)
+                include = value[index] + dp[index - 1][w - weight[index]];
+
+            int exclude = 0 + dp[index - 1][capacity];
+
+            // Storing ans in dp
+            dp[index][w] = max(exclude, include);
+        }
+    }
+    return dp[n - 1][capacity];
+}
 
 int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
 {
