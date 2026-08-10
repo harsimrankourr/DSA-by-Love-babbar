47889d5cc157964bfe4e5eba229ef3306eee7aa0
harsimrankourr <harsimrankour48@gmail.com>
2026-08-10 16:03:27 +0530

    Count derangements using Tabulation

--- Intro to C++ Programming/43.Dynamic_programming/7.Count_Derangement.cpp
+++ Intro to C++ Programming/43.Dynamic_programming/7.Count_Derangement.cpp
@@ -25,6 +25,7 @@ so the obtained solution is
 */
 
 #define MOD 1000000007
+#include <vector>
 
 long long int solveMemo(int n, vector<long long int> &dp)
 {
@@ -42,8 +43,27 @@ long long int solveMemo(int n, vector<long long int> &dp)
     return dp[n];
 }
 
+long long int solveTab(int n)
+{
+    vector<long long int> dp(n + 1, 0);
+    dp[1] = 0;
+    dp[2] = 1;
+
+    for (int i = 3; i <= n; i++)
+    {
+        long long int first = dp[i - 1] % MOD;
+        long long int second = dp[i - 2] % MOD;
+        long long int sum = (first + second) % MOD;
+
+        long long int ans = ((i - 1) * sum) % MOD;
+        dp[i] = ans;
+    }
+    return dp[n];
+}
+
 long long int countDerangements(int n)
 {
-    vector<long long int> dp(n + 1, -1);
-    return solveMemo(n, dp);
+    // vector<long long int> dp(n + 1, -1);
+    // return solveMemo(n, dp);
+    return solveTab(n);
 }
