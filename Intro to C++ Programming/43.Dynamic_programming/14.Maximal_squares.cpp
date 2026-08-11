3ff8fb49905624b092e12233dfb859bb7108bb1a
harsimrankourr <harsimrankour48@gmail.com>
2026-08-11 16:19:12 +0530

    Maximal square using Tabulation

--- Intro to C++ Programming/43.Dynamic_programming/14.Maximal_squares.cpp
+++ Intro to C++ Programming/43.Dynamic_programming/14.Maximal_squares.cpp
@@ -52,16 +52,48 @@ public:
         }
     }
 
+    int solveTab(vector<vector<int>> &mat, int &maxi)
+    {
+        int row = mat.size();
+        int col = mat[0].size();
+
+        vector<vector<int>> dp(row + 1, vector<int>(col + 1, 0));
+
+        for (int i = row - 1; i >= 0; i--)
+        {
+            for (int j = col - 1; j >= 0; j--)
+            {
+                int right = dp[i][j + 1];
+                int diagonal = dp[i + 1][j + 1];
+                int down = dp[i + 1][j + 1];
+
+                if (mat[i][j] = '1')
+                {
+                    dp[i][j] = 1 + min(right, min(diagonal, down));
+                    maxi = max(maxi, dp[i][j]);
+                }
+                else
+                {
+                    return dp[i][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
 
     int maxSquare(int n, int m, vector<vector<int>> mat)
     {
+        // // int maxi = 0;
+        // // solve(mat, 0, 0, maxi);
+        // // return maxi;
+
         // int maxi = 0;
-        // solve(mat, 0, 0, maxi);
+        // vector<vector<int>> dp(n, vector<int>(m, -1));
+        // solveMemo(mat, 0, 0, maxi, dp);
         // return maxi;
 
         int maxi = 0;
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        solveMemo(mat, 0, 0, maxi, dp);
+        salveTab(mat, maxi);
         return maxi;
     }
 };
