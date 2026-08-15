7356f28815b830b6af1e683ff5686381e1a736fa
harsimrankourr <harsimrankour48@gmail.com>
2026-08-15 11:40:08 +0530

    Partition equal subset sum using tabulation

--- Intro to C++ Programming/43.Dynamic_programming/4.Dp_with_distinct_ways/2.Partition_Equal_Subset_Sum.cpp
+++ Intro to C++ Programming/43.Dynamic_programming/4.Dp_with_distinct_ways/2.Partition_Equal_Subset_Sum.cpp
@@ -46,13 +46,37 @@ public:
         if (target == 0)
             return 1;
 
-            if(dp[index][target] != -1)
-            return dp[index][target]; 
+        if (dp[index][target] != -1)
+            return dp[index][target];
 
         bool incl = solveMemo(index + 1, arr, N, target - arr[index], dp);
         bool excl = solveMemo(index + 1, arr, N, target - 0, dp);
 
         return dp[index][target] = incl or excl;
+    }
+
+    bool solveTab(int N, int arr[], int total)
+    {
+        vector<vector<int>> dp(N + 1, vector<int>(total + 1, 0));
+
+        for (int i = 0; i <= N; i++)
+        {
+            dp[i][0] = 1;
+        }
+        for (int index = N - 1; index >= 0; index--)
+        {
+            for (int target = 0; target <= total / 2; target++)
+            {
+                bool incl = 0;
+                if (target - arr[index] >= 0)
+                    incl = dp[index + 1][target - arr[index]];
+
+                bool excl = dp[index + 1][target - 0];
+
+                dp[index][target] = incl or excl;
+            }
+        }
+        return dp[0][total / 2];
     }
 
     int equalPartition(int N, int arr[])
@@ -69,7 +93,9 @@ public:
 
         // return solve(0, arr, N, target);
 
-        vector<vector<int>> dp(N, vcetor<int>(target + 1, -1));
-        return solveMemo(0, arr, N, target, dp);
+        // vector<vector<int>> dp(N, vcetor<int>(target + 1, -1));
+        // return solveMemo(0, arr, N, target, dp);
+
+        return solveTab(N, arr, total);
     }
 };
