7edea4da681706d21b00de988aa17972a96016f1
harsimrankourr <harsimrankour48@gmail.com>
2026-08-18 17:01:02 +0530

    Best time to buy and sell stock 2 Space optimal solution

--- Intro to C++ Programming/43.Dynamic_programming/7.DP_with_stocks/2.Best_time_to_buy_and_sell_stocks11.cpp
+++ Intro to C++ Programming/43.Dynamic_programming/7.DP_with_stocks/2.Best_time_to_buy_and_sell_stocks11.cpp
@@ -1,5 +1,6 @@
 // Best timr to buy and sell stocks 11 on leetcode
 
+/*
 class Solution
 {
 public:
@@ -55,7 +56,7 @@ public:
     int solveTab(vector<int> &prices)
     {
         int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
 
         for (int index = n - 1; index >= 0; index--)
         {
@@ -66,14 +67,14 @@ public:
                 if (buy)
                 {
                     int buyIt = -prices[index] + dp[index + 1][0];
-                    int skipIt = 0 + dp[index +1][1];
+                    int skipIt = 0 + dp[index + 1][1];
 
                     profit = max(buyIt, skipIt);
                 }
                 else
                 {
-                    int sellIt = +prices[index] + dp[index+1][1];
-                    int skipIt = 0 + dp[index+1][0];
+                    int sellIt = +prices[index] + dp[index + 1][1];
+                    int skipIt = 0 + dp[index + 1][0];
 
                     profit = max(sellIt, skipIt);
                 }
@@ -83,15 +84,52 @@ public:
         return dp[0][1];
     }
 
+    int solveSo(vector<int> &prices)
+    {
+        int n = prices.size();
+
+        vector<int> curr(2, 0);
+        vector<int> next(2, 0);
+
+        for (int index = n - 1; index >= 0; index--)
+        {
+            for (int buy = 0; buy <= 1; buy++)
+            {
+
+                int profit = 0;
+                if (buy)
+                {
+                    int buyIt = -prices[index] + next[0];
+                    int skipIt = 0 + next[1];
+
+                    profit = max(buyIt, skipIt);
+                }
+                else
+                {
+                    int sellIt = +prices[index] + next[1];
+                    int skipIt = 0 + next[0];
+
+                    profit = max(sellIt, skipIt);
+                }
+                curr[buy] = profit;
+            }
+            next = curr;
+        }
+        return next[1];
+    }
+
     int maxProfit(vector<int> &prices)
     {
-        // // return solve(0, 1, prices);
+        // // // return solve(0, 1, prices);
 
-        // int n = prices.size();
-        // vector<vector<int>> dp(n, vector<int>(2, -1));
+        // // int n = prices.size();
+        // // vector<vector<int>> dp(n, vector<int>(2, -1));
 
-        // return solveMemo(0, 1, prices, dp);
+        // // return solveMemo(0, 1, prices, dp);
 
-        return solveTab(prices);
+        // return solveTab(prices);
+
+        return solveSo(prices);
     }
 };
+*/
