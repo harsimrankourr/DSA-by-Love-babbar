91bb48f77cfc3d1ec1b1ecf441617aecf1ead31c
harsimrankourr <harsimrankour48@gmail.com>
2026-08-19 14:55:39 +0530

    Best time to buy and sell stock 4 using recursion and memoization

--- Intro to C++ Programming/43.Dynamic_programming/7.DP_with_stocks/4.Best_time_to_buy_and_sell_stocks4.cpp
+++ Intro to C++ Programming/43.Dynamic_programming/7.DP_with_stocks/4.Best_time_to_buy_and_sell_stocks4.cpp
@@ -47,3 +47,75 @@ public:
 };
 
 */
+
+class Solution
+{
+public:
+    int solve(int index, int operationNo, int k, vector<int> &prices)
+    {
+        if (index == prices.size())
+            return 0;
+
+        if (operationNo == 2 * k)
+            return 0;
+
+        int profit = 0;
+
+        if (operationNo % 2 == 0)
+        {
+            // Buy allowed
+            int buyIt = -prices[index] + solve(index + 1, operationNo + 1, k, prices);
+            int skipIt = 0 + solve(index + 1, operationNo, k, prices);
+
+            profit = max(buyIt, skipIt);
+        }
+        else
+        {
+            int sellIt = +prices[index] + solve(index + 1, operationNo + 1, k, prices);
+            int skipIt = 0 + solve(index + 1, operationNo, k, prices);
+
+            profit = max(sellIt, skipIt);
+        }
+        return profit;
+    }
+
+    int solveMemo(int index, int operationNo, int k, vector<int> &prices, vector<vector<int>> &dp)
+    {
+        if (index == prices.size())
+            return 0;
+
+        if (operationNo == 2 * k)
+            return 0;
+
+        if (dp[index][operationNo] != -1)
+            return dp[index][operationNo];
+
+        int profit = 0;
+
+        if (operationNo % 2 == 0)
+        {
+            // Buy allowed
+            int buyIt = -prices[index] + solveMemo(index + 1, operationNo + 1, k, prices, dp);
+            int skipIt = 0 + solveMemo(index + 1, operationNo, k, prices, dp);
+
+            profit = max(buyIt, skipIt);
+        }
+        else
+        {
+            int sellIt = +prices[index] + solveMemo(index + 1, operationNo + 1, k, prices, dp);
+            int skipIt = 0 + solveMemo(index + 1, operationNo, k, prices, dp);
+
+            profit = max(sellIt, skipIt);
+        }
+        return dp[index][operationNo] = profit;
+    }
+
+    int maxProfit(int k, vector<int> &prices)
+    {
+        // return solve(0, 0, k, prices);
+
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2 * k, -1));
+        return solveMemo(0, 0, k, prices, dp);
+    }
+};
