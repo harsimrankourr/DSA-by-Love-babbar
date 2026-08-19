0f85cf2c4849479fb2c7b4508d9dafbc8bc37b7f
harsimrankourr <harsimrankour48@gmail.com>
2026-08-19 17:30:51 +0530

    Reverse words in a given string

---
+++ Intro to C++ Programming/44.Greedy_Algorithm/4.Reverse_words_in_a_given_string.cpp
@@ -0,0 +1,28 @@
+/*
+class Solution
+{
+public:
+    string reverseWords(string S)
+    {
+        string ans = " ";
+        string temp = " ";
+        for (int i = S.length() - 1; i >= 0; i--)
+        {
+            if (S[i] == '.')
+            {
+                reverse(temp.begin(), temp.end());
+                ans = ans + temp;
+                ans.push_back('.');
+                temp = " ";
+            }
+            else
+            {
+                temp.push_back(S[i]);
+            }
+        }
+        reverse(temp.begin(), temp.end());
+        ans = ans + temp;
+        return ans;
+    }
+};
+*/
