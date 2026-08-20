08f9a53b69641a541e814a65db189430b5501453
harsimrankourr <harsimrankour48@gmail.com>
2026-08-20 15:56:21 +0530

    Maximum sum of subarray of size k

--- Intro to C++ Programming/45.Slinding_Window/1.Maximum_sum_subarray_of_size_k.cpp
+++ Intro to C++ Programming/45.Slinding_Window/1.Maximum_sum_subarray_of_size_k.cpp
@@ -1,30 +1,44 @@
-/*
-class Solution
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxSum(vector<int> &arr, int k)
 {
-public:
-    int maxSubarraySum(vector<int> &arr, int k)
+    int n = arr.size();
+
+    // Step 1: Calculate sum of first window
+    int windowSum = 0;
+
+    for (int i = 0; i < k; i++)
     {
-        int i = 0;
-        int j = 0;
-        int sum = 0;
-        int maxi = INT_MIN;
-
-        while (j < size.arr())
-        {
-            sum = sum + arr[j];
-
-            if ((j - i) + 1 < k)
-                j++;
-
-            else if ((j - i) + 1 == k)
-                maxi = max(maxi, sum);
-
-            sum = sum - arr[i];
-            i++;
-            j++;
-        }
-        return maxi;
+        windowSum += arr[i];
     }
-};
 
-*/
+    int maxSum = windowSum;
+
+    // Step 2: Slide the window
+    for (int i = k; i < n; i++)
+    {
+        // Remove the element leaving the window
+        windowSum -= arr[i - k];
+
+        // Add the new element entering the window
+        windowSum += arr[i];
+
+        // Update maximum
+        maxSum = max(maxSum, windowSum);
+    }
+
+    return maxSum;
+}
+
+int main()
+{
+    vector<int> arr = {2, 1, 5, 1, 3, 2};
+
+    int k = 3;
+
+    cout << maxSum(arr, k) << endl;
+
+    return 0;
+}
